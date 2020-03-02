f1e8e12fa29b3c0623301e3267319c1bd5f74147
jelleas <jelle.van.assema@gmail.com>
2020-03-02 16:32:36 +0100

    refactor check_jupyter

--- mod1/__init__.py
+++ mod1/__init__.py
@@ -11,7 +11,8 @@ def exists():
 def test1():
     """Correcte antwoord bij vraag 1"""
     check_jupyter = check50.internal.import_file("check_jupyter", "check_jupyter.py")
-    results = check_jupyter.execute_up_to("module 1.ipynb", "vraag1")
+    cells = check_jupyter.cells_up_to("module 1.ipynb", "vraag1")
+    results = check_jupyter.execute(cells)
     output = check_jupyter.output_from_cell(results[-1])
 
     for type, val in [("sales", 39041), ("ads", 8702), ("subscriptions", 13200), ("donations", 292)]:
@@ -19,15 +20,32 @@ def test1():
         if not re.search(regex, output):
             raise check50.Failure(f"Did not find {type} {val} in output:\n{output}")
 
-
 @check50.check(exists)
 def test2():
     """Correcte antwoord bij vraag 2"""
     check_jupyter = check50.internal.import_file("check_jupyter", "check_jupyter.py")
-    results = check_jupyter.execute_up_to("module 1.ipynb", "vraag2")
+    cells = check_jupyter.cells_up_to("module 1.ipynb", "vraag2")
+    results = check_jupyter.execute(cells)
     output = check_jupyter.output_from_cell(results[-1])
 
     answer = 28938
     regex = re.compile(str(answer), re.MULTILINE)
     if not re.search(regex, output):
-        raise check50.Failure("Did not find {answer} in output:\n{output")
+        raise check50.Failure(f"Did not find {answer} in output:\n{output}")
+
+@check50.check(exists)
+def test3():
+    """Check of een variabele (a) de waarde 'b' bevat"""
+    # TODO remove (just import check_jupyter)
+    check_jupyter = check50.internal.import_file("check_jupyter", "check_jupyter.py")
+
+    # Grab all cells upto vraag2
+    cells = check_jupyter.cells_up_to("module 1.ipynb", "vraag2")
+
+    # Create a cell to perform an assertion
+    check_cell = check_jupyter.create_cell("assert a == 'b'")
+
+    # Execute all cells + our assertion
+    with check_jupyter.executor() as execute:
+        execute(cells)
+        result_cell = execute(check_cell)
--- mod1/check_jupyter.py
+++ mod1/check_jupyter.py
@@ -1,7 +1,13 @@
 import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
+import check50
+from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 
-def execute_up_to(notebook_path, cell_id):
+import contextlib
+
+def cells_up_to(notebook_path, cell_id):
+    """
+    Grab all cells from notebook_path upto cell_id
+    """
     # Open and parse the notebook
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
@@ -17,22 +23,65 @@ def execute_up_to(notebook_path, cell_id):
     else:
         raise Exception(f"Cell with ID:{cell_id} not found.")
 
+    return cells
+
+
+def create_cell(code):
+    """
+    Create a new cell
+    """
+    return nbformat.notebooknode.from_dict(
+        {"source": code,
+         "cell_type": "code",
+         "metadata": {}
+        })
+
+
+def execute(cells):
+    """
+    Execute all cells
+    """
+    with executor() as execute:
+        return execute(cells)
+
+
+@contextlib.contextmanager
+def executor():
+    """
+    Creates an executor context in which any call to the executor
+    is executed in sequence in the same kernel. For example:
+    with executor() as execute:
+        execute(create_cell("a = 3"))
+        execute(create_cell("print(a)"))
+    """
+    def execute(cells):
+        # If just one cell is passed, put it in a list
+        if not isinstance(cells, list) and not isinstance(cells, tuple):
+            cells = [cells]
+
+        # Execute all cells
+        results = []
+        for index, cell in enumerate(cells):
+            try:
+                results.append(ep.preprocess_cell(cell, {}, index))
+            except CellExecutionError as e:
+                raise check50.Failure(str(e))
+        
+        return results
+
     # Start an ExecutePreprocessor: https://nbconvert.readthedocs.io/en/latest/execute_api.html
     # https://github.com/jupyter/nbconvert/blob/master/nbconvert/preprocessors/execute.py
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
 
-    results = []
-
     # Start a Kernel Manager
     with ep.setup_preprocessor(None, {}):
-        for index, cell in enumerate(cells):
-            result = ep.preprocess_cell(cell, {}, index)
-            results.append(result)
-
-    return results
+        yield execute
 
 
 def output_from_cell(cell):
+    """
+    Grabs the output from a cell.
+    """
     output_data = cell[0]["outputs"][0]
 
     if output_data["output_type"] == "stream":
